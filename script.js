1779cb4bb6496373c3970ceb14f60ef3e6b47001
esport-restasorkot <cc2025sorkot@gmail.com>
2025-05-30 21:55:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // JSON Data
 const standingsData = [
-    { rank: 1, team: "AANSLAG SQUAD", matches: 1, wins: 2, losses: 1, pts: "1", points: 2 },
-    { rank: 2, team: "VALOR HEXA SQUAD", matches: 1, wins: 2, losses: 1, pts: "2", points: 2 },
-    { rank: 3, team: "BASTARD SQUAD", matches: 1, wins: 1, losses: 2, pts: "3", points: 1 },
-    { rank: 4, team: "BAJIR SQUAD", matches: 1, wins: 1, losses: 2, pts: "4", points: 1 }
+    { rank: 1, team: "AANSLAG SQUAD", matches: 2, wins: 4, losses: 1, pts: "1", points: 4 },
+    { rank: 2, team: "VALOR HEXA SQUAD", matches: 2, wins: 4, losses: 1, pts: "2", points: 4 },
+    { rank: 3, team: "BASTARD SQUAD", matches: 2, wins: 1, losses: 4, pts: "3", points: 1 },
+    { rank: 4, team: "BAJIR SQUAD", matches: 2, wins: 1, losses: 4, pts: "4", points: 1 }
     ];
 
 const topPlayersData = [
